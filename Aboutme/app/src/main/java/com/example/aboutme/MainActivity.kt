2ec182d793ff62af76d20d57db162a84c3fde9e1
Khaled Saad <65211124+khaled45690@users.noreply.github.com>
2022-12-03 05:50:32 +0200

    ..

--- Aboutme/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ Aboutme/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.aboutme
 
-import android.content.ContentValues.TAG
-import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
 import android.util.Log
 import android.view.View
 import com.example.aboutme.databinding.ActivityMainBinding
@@ -13,21 +10,22 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("Info", "addNickName: ")
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
-        binding.addNickNameButton.setOnClickListener {
+        setContentView(binding.root)
+        binding.ClickME.setOnClickListener {
             addNickName(it)
         }
     }
 
     private fun addNickName(view:View) {
         binding.apply {
-            binding.nickName.text = binding.textInputEditText.text.toString()
-            Log.d(TAG, "addNickName: " + binding.textInputEditText.text.toString())
-            invalidateOptionsMenu()
-            binding.addNickNameButton.visibility = View.GONE
-            binding.textInputEditText.visibility = View.GONE
-            binding.nickName.visibility = View.VISIBLE
+            Log.d("Info", "addNickName: ")
+//            binding.nickName.text = binding.textInputEditText.text.toString()
+//            Log.d("Info", "addNickName: " + binding.textInputEditText.text.toString())
+//            binding.addNickNameButton.visibility = View.GONE
+//            binding.textInputEditText.visibility = View.GONE
+//            binding.nickName.visibility = View.VISIBLE
         }
     }
 }
